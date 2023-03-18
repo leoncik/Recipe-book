b24e58bc0ed9435d869ab79d2dc91db2ed912fe9
Léonard Wojcik <leonardw@tutanota.com>
2023-03-18 17:30:15 +0100

    feat: display a list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+    recipes: Recipe[] = [
+        new Recipe(
+            'Pierogi',
+            'Delicious Polish dumplings.',
+            'https://upload.wikimedia.org/wikipedia/commons/b/ba/04565_Christmas_dumplings_with_dried_plums.JPG'
+        ),
+        new Recipe(
+            'Tomato soup',
+            'Tomato soup is a soup with tomatoes. What a suprise.',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Tomato_soup.jpg/800px-Tomato_soup.jpg'
+        ),
+    ];
 }
