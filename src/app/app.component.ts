3002e125fb200f04682725d0774ccd0cb14c5e98
Léonard Wojcik <leonardw@tutanota.com>
2023-03-19 15:30:39 +0100

    feat: add navigation with event binding - This is only to practice event binding. Real touting solution will be used later.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+    loadedPage = 'recipes';
+
+    onNavigate(page: string) {
+        this.loadedPage = page;
+    }
 }
