ec7a3fed0d6306ec45c6c2bb07849fa7deebe461
Léonard Wojcik <leonardw@tutanota.com>
2023-03-19 10:51:14 +0100

    feat: setup shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 4),
+        new Ingredient('Potatoes', 6),
+    ];
 }
