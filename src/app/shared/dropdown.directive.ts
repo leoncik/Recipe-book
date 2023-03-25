0991287d7c54fdb155ba2b8c91dd85a54d86bd7b
Léonard Wojcik <leonardw@tutanota.com>
2023-03-25 15:59:54 +0100

    feat: close dropdown if click outside of toggler

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -11,7 +11,16 @@ import {
 export class DropdownDirective {
     @HostBinding('class.dropdown-open') isOpen = false;
 
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
+    // Toggles the dropdown when clicking on toggle button
+    // @HostListener('click') toggleOpen() {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    // Toggles dropdown when clicking on toggle button and closes It when clicking outside of It
+    constructor(private elRef: ElementRef) {}
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target)
+            ? !this.isOpen
+            : false;
     }
 }
