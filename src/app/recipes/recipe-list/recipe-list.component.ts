3c13eddeb75581f863460a6992ec09a598dd0997
Léonard Wojcik <leonardw@tutanota.com>
2023-03-26 15:03:06 +0200

    refactor: use service to set selected recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,13 +8,7 @@ import { RecipeService } from '../recipe.service';
     styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-    @Output() selectedRecipeAgain = new EventEmitter<Recipe>();
-
     recipes!: Recipe[];
-
-    onSelectRecipeAgain(recipe: Recipe) {
-        this.selectedRecipeAgain.emit(recipe);
-    }
 
     constructor(private recipeService: RecipeService) {}
 
