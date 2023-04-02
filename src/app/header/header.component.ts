56c9d25b06994f86e46da2bba75eccc57a466ed1
Léonard Wojcik <leonardw@tutanota.com>
2023-04-02 13:58:59 +0200

    refactor: use proper routing for navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-    @Output() pageSelected = new EventEmitter<string>();
-
-    onSelect(page: string) {
-        this.pageSelected.emit(page);
-    }
-}
+export class HeaderComponent {}
