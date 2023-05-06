dbb8cf1f6d4dfe1b9ec709812921a697e7754420
Léonard Wojcik <leonardw@tutanota.com>
2023-05-06 10:44:09 +0200

    refactor: remove unused method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-    loadedPage = 'recipes';
-
-    onNavigate(page: string) {
-        this.loadedPage = page;
-    }
-}
+export class AppComponent {}
