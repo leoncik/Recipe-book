be281241be2148c6751256968bdbc76727815e76
Léonard Wojcik <leonardw@tutanota.com>
2023-05-27 17:53:08 +0200

    refactor: replace mocked data with fetched data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,25 +12,26 @@ export class RecipeService {
     // to reflect changes on page
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Pierogi',
-            'Delicious Polish dumplings.',
-            'https://upload.wikimedia.org/wikipedia/commons/b/ba/04565_Christmas_dumplings_with_dried_plums.JPG',
-            [
-                new Ingredient('Eggs', 2),
-                new Ingredient('Water', 150),
-                new Ingredient('Flour', 500),
-                new Ingredient('Potatoes', 8),
-            ]
-        ),
-        new Recipe(
-            'Tomato soup',
-            'Tomato soup is a soup with tomatoes. What a suprise.',
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Tomato_soup.jpg/800px-Tomato_soup.jpg',
-            [new Ingredient('Tomatoes', 4), new Ingredient('Oil', 1)]
-        ),
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Pierogi',
+    //         'Delicious Polish dumplings.',
+    //         'https://upload.wikimedia.org/wikipedia/commons/b/ba/04565_Christmas_dumplings_with_dried_plums.JPG',
+    //         [
+    //             new Ingredient('Eggs', 2),
+    //             new Ingredient('Water', 150),
+    //             new Ingredient('Flour', 500),
+    //             new Ingredient('Potatoes', 8),
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'Tomato soup',
+    //         'Tomato soup is a soup with tomatoes. What a suprise.',
+    //         'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Tomato_soup.jpg/800px-Tomato_soup.jpg',
+    //         [new Ingredient('Tomatoes', 4), new Ingredient('Oil', 1)]
+    //     ),
+    // ];
+    private recipes: Recipe[] = [];
 
     setRecipes(recipes: Recipe[]) {
         this.recipes = recipes;
