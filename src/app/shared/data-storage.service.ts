9886fdcf1a49333b81e7824e939a0c920a7be669
Léonard Wojcik <leonardw@tutanota.com>
2023-05-27 17:54:32 +0200

    feat: prevent bugs if there are no ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -29,6 +30,18 @@ export class DataStorageService {
     fetchRecipes() {
         this.http
             .get<Recipe[]>(`${this.baseUrl}/recipes.json`)
+            .pipe(
+                map((recipes) => {
+                    return recipes.map((recipe) => {
+                        return {
+                            ...recipe,
+                            ingredients: recipe.ingredients
+                                ? recipe.ingredients
+                                : [],
+                        };
+                    });
+                })
+            )
             .subscribe((recipes) => {
                 this.recipeService.setRecipes(recipes);
             });
