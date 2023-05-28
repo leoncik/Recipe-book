d666e16039604e1e5d1377ac7cbeec2ab52fb403
Léonard Wojcik <leonardw@tutanota.com>
2023-05-28 17:57:29 +0200

    feat: redirect user after login or register

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent {
     isLoading = false;
     error: string | null = null;
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     onSwitchMode(): void {
         this.isLoginMode = !this.isLoginMode;
@@ -31,7 +32,6 @@ export class AuthComponent {
 
         this.isLoading = true;
         if (this.isLoginMode) {
-            console.log('en mode login');
             authObservable = this.authService.login(email, password);
         } else {
             authObservable = this.authService.register(email, password);
@@ -39,8 +39,8 @@ export class AuthComponent {
 
         authObservable.subscribe(
             (responseData) => {
-                console.log(responseData);
                 this.isLoading = false;
+                this.router.navigate(['/recipes']);
             },
             (errorMessage) => {
                 console.log(errorMessage);
