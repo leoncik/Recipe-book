aaa8ba7fe0308b42487c5c0b86ec04defbbb3da8
Léonard Wojcik <leonardw@tutanota.com>
2023-05-28 15:01:04 +0200

    feat: setup login request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({
@@ -43,5 +44,16 @@ export class AuthService {
                     return throwError(errorMessage);
                 })
             );
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBDT0jYJtP7RGjAUTEwjQyTlHpw4AnqQZc',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true,
+            }
+        );
     }
 }
