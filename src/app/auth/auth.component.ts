81e092b963d6aa8b4e17afc19354ea72ce35c68b
Léonard Wojcik <leonardw@tutanota.com>
2023-05-28 10:33:20 +0200

    feat: register user

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -9,12 +10,32 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent {
     isLoginMode = false;
 
+    constructor(private authService: AuthService) {}
+
     onSwitchMode(): void {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm): void {
-        console.log(form.value);
+        // Add an extra check if user manages to bypass the disabled submit button.
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        if (this.isLoginMode) {
+            console.log('logged in!');
+        } else {
+            this.authService.register(email, password).subscribe(
+                (responseData) => {
+                    console.log(responseData);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        }
         form.reset();
     }
 }
