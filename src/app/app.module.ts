feea34f6a89c5ca6c9ca52b42054efc58a75067a
Léonard Wojcik <leonardw@tutanota.com>
2023-06-03 14:59:50 +0200

    refactor: create a shopping-list module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AuthComponent } from './auth/auth.component';
@@ -14,13 +12,12 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        ShoppingListComponent,
-        ShoppingEditComponent,
         DropdownDirective,
         AuthComponent,
         LoadingSpinnerComponent,
@@ -33,6 +30,7 @@ import { RecipesModule } from './recipes/recipes.module';
         ReactiveFormsModule,
         HttpClientModule,
         RecipesModule,
+        ShoppingListModule,
     ],
     providers: [
         {
