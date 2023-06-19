623a0470b0b655d064b34b52f97730930608d416
Léonard Wojcik <leonardw@tutanota.com>
2023-06-19 14:48:40 +0200

    fix: ingredient model typing

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,11 +1,7 @@
 type IWeight = 'g' | 'kg' | 'mg';
 type ILength = 'mm' | 'cm' | 'm';
 type IQuantity = 'pcs';
-
-interface IVolume {
-    name: 'Volume';
-    value: 'mL' | 'L' | 'dL' | 'tsp';
-}
+type IVolume = 'mL' | 'L' | 'dL' | 'tsp';
 
 type IUnit = IVolume | IWeight | ILength | IQuantity;
 
