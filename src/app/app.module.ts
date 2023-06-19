5ef1c960d113892b439587706b70c3be5aed2013
Léonard Wojcik <leonardw@tutanota.com>
2023-06-19 18:23:38 +0200

    feat: add style to the header with material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +30,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         ReactiveFormsModule,
         HttpClientModule,
         BrowserAnimationsModule,
+        MatListModule,
     ],
     providers: [
         {
